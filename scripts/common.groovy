3256bbd251486b8f3840b46d2cd4217a15c1a55d
shariff-edx <n.shaik@edexa.team>
2024-11-15 23:01:48 +0530

    shared-libraries

--- scripts/common.groovy
+++ scripts/common.groovy
@@ -21,10 +21,14 @@
 // return this
 def writeEnvToFile(workspace, copyArtifact, envVars) {
     if (!copyArtifact) {
-        def filePath = "${workspace}/spawn/env_list.txt"
+        def spawnDir = "${workspace}/spawn"
+        def filePath = "${spawnDir}/env_list.txt"
         def envContent = envVars.collect { key, value ->
             "${key}=${value}"
         }.join("\n") // Convert the map to "key=value" lines
+
+        // Ensure the directory exists
+        new File(spawnDir).mkdirs()
 
         writeFile file: filePath, text: envContent
     }
