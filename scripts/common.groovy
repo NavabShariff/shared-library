7f2966c9a222eec2c015df8266610005200670f9
shariff-edx <n.shaik@edexa.team>
2024-11-15 23:04:12 +0530

    shared-libraries

--- scripts/common.groovy
+++ scripts/common.groovy
@@ -1,24 +1,3 @@
-// def writeEnvToFile(workspace, copyArtifact) {
-//     sh """
-//     if [ "${copyArtifact}" = "false" ]; then
-//         mkdir -p ${workspace}/spawn
-//         rm -rf ${workspace}/spawn/env_list.txt
-//         printf "BUILD_NUMBER=${env.BUILD_NUMBER}\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "JOB_NAME=${env.JOB_NAME}\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "BUILD_URL=${env.BUILD_URL}\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "WORKSPACE=${workspace}\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "DASH_UID=\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "AWS_ACCESS_KEY_ID=\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "AWS_SECRET_ACCESS_KEY=\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "AWS_DEFAULT_REGION=\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "AWS_REGION=\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "AWS_ZONE=\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "AWS_INSTANCE_NAME=\\n" >> ${workspace}/spawn/env_list.txt
-//         printf "AWS_INSTANCE_TYPE=\\n" >> ${workspace}/spawn/env_list.txt
-//     fi
-//     """
-// }
-// return this
 def writeEnvToFile(workspace, copyArtifact, envVars) {
     if (!copyArtifact) {
         def spawnDir = "${workspace}/spawn"
@@ -33,3 +12,4 @@ def writeEnvToFile(workspace, copyArtifact, envVars) {
         writeFile file: filePath, text: envContent
     }
 }
+
