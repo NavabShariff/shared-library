81b76c83b44633e2960663358434f49f98fc4059
shariff-edx <n.shaik@edexa.team>
2024-11-16 00:25:14 +0530

    shared

--- scripts/common.groovy
+++ scripts/common.groovy
@@ -4,39 +4,40 @@ def writeEnvToFile(workspace, copyArtifact) {
         mkdir -p ${workspace}/spawn
         rm -rf ${workspace}/spawn/env_list.txt
         printf "
-BUILD_NUMBER="{env.BUILD_NUMBER}"
-JOB_NAME="{env.JOB_NAME}"
-BUILD_URL="{env.BUILD_URL}"
-WORKSPACE="{env.WORKSPACE}"
-DASH_UID="navab"
-AWS_DEFAULT_REGION="pradeepa"
-AWS_ACCESS_KEY_ID="Navab"
-AWS_SECRET_ACCESS_KEY=""
-AWS_REGION="ap-south-1"
-AWS_ZONE="ap-south-1a"
-AWS_INSTANCE_NAME="spawn"
-AWS_INSTANCE_TYPE="t2.micro"
+BUILD_NUMBER
+JOB_NAME
+BUILD_URL
+WORKSPACE
+DASH_UID
+AWS_DEFAULT_REGION
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_REGION
+AWS_ZONE
+AWS_INSTANCE_NAME
+AWS_INSTANCE_TYPE
 " > ${workspace}/spawn/env_list.txt
     fi
     """
 }
 
-return this
+// return this
 
 
 def cleanVcenter(Map args) {
     echo "Cleaning with args: ${args}"
     
-    // Test command with a simpler image (nginx or ubuntu)
+
     def command = """
     docker run --rm \
     --env-file ${args.WORKSPACE}/spawn/env_list.txt \
     -v ${args.WORKSPACE}:${args.WORKSPACE} \
     -v /var/run/docker.sock:/var/run/docker.sock ${args.SPAWN_IMAGE} \
-    /bin/bash -c "printenv"
+    spawn --verbose --config-dir ${args.WORKSPACE}/spawn \
+    clean vcenter \
+    --vcenter-name "${args.VCENTER_NAME}"
     """
     
-    // Run the command (this will print "Hello from <image_name>" to check if the container is running)
     sh command
 }
 
