a780ce706b04f7c4dba82a56163b55a80a31c69f
shariff-edx <n.shaik@edexa.team>
2024-11-15 23:26:43 +0530

    shared-libraries

--- scripts/common.groovy
+++ scripts/common.groovy
@@ -1,25 +1,24 @@
-def writeEnvToFile(Map args) {
-    println "writeEnvToFile"
-    println args
-
-    def workspace = args.WORKSPACE
-    def copyArtifact = args.COPY_ARTIFACT
-    def envVars = args.ENV_VARS ?: []
-
-    if (copyArtifact == "false") {
-        def envContent = ""
-
-        // Dynamically build the environment variables content
-        envVars.each { varName ->
-            def varValue = env.getProperty(varName) ?: ''
-            envContent += "${varName}=${s}\n"
-        }
-
-        // Write to file
-        sh """
+def writeEnvToFile(workspace, copyArtifact) {
+    sh """
+    if [ "${copyArtifact}" = "false" ]; then
         mkdir -p ${workspace}/spawn
         rm -rf ${workspace}/spawn/env_list.txt
-        printf "${envContent}" >> ${workspace}/spawn/env_list.txt
-        """
-    }
+        envListFile="${workspace}/spawn/env_list.txt"
+        printf "
+BUILD_NUMBER
+JOB_NAME
+BUILD_URL
+WORKSPACE
+DASH_UID
+AWS_DEFAULT_REGION
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_REGION
+AWS_ZONE
+AWS_INSTANCE_NAME
+AWS_INSTANCE_TYPE
+" > $envListFile
+    fi
+    """
 }
+return this
