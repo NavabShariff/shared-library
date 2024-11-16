8baad55ef73f57f561ff09b85b7296c6c1338d36
shariff-edx <n.shaik@edexa.team>
2024-11-16 13:59:28 +0530

    shared

--- scripts/common.groovy
+++ scripts/common.groovy
@@ -84,3 +84,13 @@ def getNginxContainerId() {
     echo "NGINX container started with ID: ${containerId}"
     return containerId
 }
+
+def removeNginxContainer(containerId) {
+    echo "Stopping and removing NGINX container with ID: ${containerId}"
+    
+    // Stop and remove the container using docker rm -f
+    sh(script: "docker rm -f ${containerId}")
+    echo "NGINX container with ID ${containerId} has been removed."
+}
+
+return this
